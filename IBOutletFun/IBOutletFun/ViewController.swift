b3fb9510b1b416e5177a09a83cae7f110b81c7b3
jellybeanjohnny <mamerige@icloud.com>
2016-07-06 16:19:23 -0400

    completed lab

--- IBOutletFun/IBOutletFun/ViewController.swift
+++ IBOutletFun/IBOutletFun/ViewController.swift
@@ -9,14 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	@IBOutlet weak var textDisplay: UILabel!
+	
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+			textDisplay.text = "Zzz..."
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+	
+	@IBAction func helloWord(sender: AnyObject) {
+		
+		print("Pressed Hello Word button")
+		textDisplay.text = "Hello Word"
+		
+	}
+	
+	
+	@IBAction func goodbyeWorld(sender: AnyObject) {
+		
+		print("Pressed Goodbye World button")
+		textDisplay.text = "Goodbye World"
+		
+	}
+	
+	@IBAction func unicorn(sender: AnyObject) {
+		
+		print("Unicorn button pressed")
+		textDisplay.text = "x"
+		
+	}
+
+	
 }
